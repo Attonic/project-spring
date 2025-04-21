3d8bcd2d2f07868fc5d9144c70dc8da5879a0591
attonic <a.cleisonn@gmail.com>
2025-04-21 15:51:09 -0300

    Add: Adicioana Classes OrderService, OrderReource para o controle rest, OrderRepository que extende da interface JpaRepository, utilizado o @JsonIgonre.

---
+++ src/main/java/com/attonic/projectspring/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.attonic.projectspring.services;
+
+import com.attonic.projectspring.entities.Order;
+import com.attonic.projectspring.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll(){
+        return repository.findAll();
+    }
+
+    public Order findById(Long id){
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
