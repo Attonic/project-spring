c6a675ba0bb050451b5599eb545f7d06ab15cecd
attonic <a.cleisonn@gmail.com>
2025-04-21 19:24:23 -0300

    Add: Adiciona o pacote e classe enum, OrderStatus, define os estatus no enum e a regra para que seja organizado por numeros

--- src/main/java/com/attonic/projectspring/config/TestConfig.java
+++ src/main/java/com/attonic/projectspring/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.attonic.projectspring.config;
 
 import com.attonic.projectspring.entities.Order;
 import com.attonic.projectspring.entities.User;
+import com.attonic.projectspring.entities.enums.OrderStatus;
 import com.attonic.projectspring.repositories.OrderRepository;
 import com.attonic.projectspring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brow", "maria@gmail.com", "988888855", "202030");
         User u2 =  new User(null, "Alex Green", "alex@gmail.com", "894949494", "212222");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WATTING_PAYMENT,u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.CANCELED ,u1);
 
 
 
--- src/main/java/com/attonic/projectspring/entities/Order.java
+++ src/main/java/com/attonic/projectspring/entities/Order.java
@@ -1,5 +1,6 @@
 package com.attonic.projectspring.entities;
 
+import com.attonic.projectspring.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -18,6 +19,7 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
     //ManyToOne é que haverá vários pedidos para um User, cliente
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -27,10 +29,12 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
-        this.client = client;
         this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+
     }
 
     public User getClient() {
@@ -55,6 +59,17 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
+
     }
 
     @Override
