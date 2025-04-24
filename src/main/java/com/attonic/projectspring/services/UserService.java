350d0cdd733d4cf5008f97244fd1006d0becdc55
attonic <a.cleisonn@gmail.com>
2025-04-24 20:38:45 -0300

    Add: Adicionado classe databaseException e feito as excessões da opão de delete de usuario, Exception handling - delete

--- src/main/java/com/attonic/projectspring/services/UserService.java
+++ src/main/java/com/attonic/projectspring/services/UserService.java
@@ -2,8 +2,11 @@ package com.attonic.projectspring.services;
 
 import com.attonic.projectspring.entities.User;
 import com.attonic.projectspring.repositories.UserRepository;
+import com.attonic.projectspring.services.exceptions.DataBaseException;
 import com.attonic.projectspring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -31,7 +34,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DataBaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
---
+++ src/main/java/com/attonic/projectspring/services/exceptions/DataBaseException.java
@@ -0,0 +1,8 @@
+package com.attonic.projectspring.services.exceptions;
+
+public class DataBaseException extends RuntimeException{
+
+    public DataBaseException(String msg){
+        super(msg);
+    }
+}
